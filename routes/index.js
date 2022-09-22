e5cbd692a351d55bdea3ed5bedbd6bb87839fd97
Basil Aslam <basilaslam@pop-os.localdomain>
2022-09-22 22:29:37 +0530

    login completed

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,44 @@
 var express = require('express');
+const app = require('../app');
 var router = express.Router();
+
+const password1 = 'basil'
+const email1= 'basil@gmail.com';
+
+
+
+
+// Functions
+function checkUser(email,password){
+  console.log('checkuser');
+  if(email == email1){
+    if(password == password1){
+      return true;
+    }
+  }
+
+  return false;
+}
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
   let arr = [{name:'a'},{name:'b'},{name:'c'}];
   res.render('login', { title: arr });
 });
+
+// Get login Details
+
+router.post('/login',(req, res)=>{
+  console.log('post');
+  if(checkUser(req.body.email,req.body.password)){
+    console.log('logged');
+  }else{
+    console.log('not logged');
+  }
+  console.log(req.body.password,req.body.email);
+})
+
+
+
 
 module.exports = router;
